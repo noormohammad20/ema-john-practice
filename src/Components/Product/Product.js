1d1d7eb62fa9aed09d5be4be3e54763d9510b506
noormohammad20 <mdsaim204@gmail.com>
2022-03-24 13:53:58 +0600

    cart css added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,7 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6 className='product-name'>{name}</h6>
+            <div className="product-info">
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p><small>Seller:{seller}</small></p>
+                <p><small>Ratings {ratings}stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     )
 }
