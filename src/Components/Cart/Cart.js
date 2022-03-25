a56d8af143f38e19be9e3ae5613346e97a41bf3c
noormohammad20 <mdsaim204@gmail.com>
2022-03-25 15:58:14 +0600

    Add to the cart with quantity and explanation

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,20 +4,22 @@ const Cart = ({ cart }) => {
 
     let total = 0
     let shipping = 0
+    let quantity = 0
     for (const product of cart) {
-        total = total + product.price
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity
         shipping = shipping + product.shipping
     }
     const tax = parseFloat((total * 0.1).toFixed(2))
-    const grandTotal = (total + shipping + tax).toFixed(2)
+    const grandTotal = total + shipping + tax
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: ${tax}</p>
-            <h5>Grand Total: ${grandTotal}</h5>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     )
 }
