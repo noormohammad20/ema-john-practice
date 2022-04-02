6324c6aab8c31c44b81dca447de38c74ed88ae35
noormohammad20 <mdsaim204@gmail.com>
2022-04-02 23:41:10 +0600

    create reveiw item component for show items in order component

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import useCart from '../../hooks/useCart'
 import useProducts from '../../hooks/useProducts'
+import Cart from '../Cart/Cart'
+import ReviewItem from '../ReviewItem/ReviewItem'
 
 const Orders = () => {
-    const [products, setProducts] = useProducts([])
-    const [cart, setCart] = useCart(products)
+    const [products] = useProducts([])
+    const [cart] = useCart(products)
     return (
-        <div>
-            <h1>{products.length}</h1>
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+
         </div>
     )
 }
---
+++ src/Components/ReviewItem/ReviewItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ReviewItem = ({ product }) => {
+    console.log(product)
+    const { name, price, shipping, quantity } = product
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ReviewItem
