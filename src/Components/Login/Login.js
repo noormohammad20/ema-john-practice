45e3bfd3253e1bfdab8d7535755d16377cbd9273
noormohammad20 <mdsaim204@gmail.com>
2022-04-12 01:46:39 +0600

    login form create

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Login.css'
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Login</h2>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="Email" id="email" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="Password" id="password" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>
+                    New to ema-john? <Link className='form-link' to='signup'>Create an account</Link>
+                </p>
+            </div>
         </div>
     )
 }
