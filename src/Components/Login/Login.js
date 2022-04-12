87566f0e52130237fbd9d52d5dcc17a51ed072b5
noormohammad20 <mdsaim204@gmail.com>
2022-04-12 17:41:39 +0600

    implement require auth

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import auth from '../../firebase.init'
 import './Login.css'
 
@@ -15,6 +15,9 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth)
     const navigate = useNavigate()
+    const location = useLocation()
+
+    const from = location.state?.from?.pathname || "/"
 
     const handleEmailBlur = e => {
         setEmail(e.target.value)
@@ -24,7 +27,7 @@ const Login = () => {
     }
 
     if (user) {
-        navigate('/shop')
+        navigate(from, { replace: true })
     }
 
     const handleUserLogin = e => {
